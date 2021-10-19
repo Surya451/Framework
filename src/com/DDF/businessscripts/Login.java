54a626e714ecab902dace039a96b32491a96992b
Surya <suryanarayanamachetti@gmail.com>
2021-10-19 17:38:54 +0530

    This is my second commit

--- src/com/DDF/businessscripts/Login.java
+++ src/com/DDF/businessscripts/Login.java
@@ -1,25 +1,30 @@
 package com.DDF.businessscripts;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import com.DDF.base.BaseTest;
+import com.DDF.utilities.Screenshot;
 
 public class Login extends BaseTest{
 
 	@Test
-	public void loginTest() {
+	public void loginTest() throws IOException {
 		
 		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
 		
-		//driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		Screenshot.takeScreenshot();
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
 		
-		
+		Screenshot.takeScreenshot();
 		
 		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		Screenshot.takeScreenshot();
 		
 		driver.findElement(By.id("btnLogin")).click();
-
+		Screenshot.takeScreenshot();
 	}
 	
 	
--- src/com/DDF/utilities/Screenshot.java
+++ src/com/DDF/utilities/Screenshot.java
@@ -1,15 +1,31 @@
 package com.DDF.utilities;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import com.DDF.base.BaseTest;
+import com.google.common.io.Files;
+
 public class Screenshot {
 
 	
 	
-	public static void takeScreenshot() {
+	public static String takeScreenshot() throws IOException {
          
-		
-		
-		
-		
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = (TakesScreenshot)BaseTest.driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String destination = "./target/" + dateName+ ".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source, finalDestination);
+		return destination;
+	
 		
 	}
 	
